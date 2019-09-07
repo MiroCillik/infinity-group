a21d8965b1ee80937b1a5b4cc90c95cb2f3821d6
Miroslav Čillík <miro@keboola.com>
2019-09-07 12:26:31 +0200

    carousel and texture bg

--- content/products.md
+++ content/products.md
@@ -2,3 +2,4 @@
 title = "Produkty"
 +++
 
+{{< gallery dir="/img/product-images/" />}}`
--- docs/js/front.js
+++ docs/js/front.js
@@ -122,9 +122,9 @@ function sliders () {
     $('.homepage').owlCarousel({
       navigation: false, // Show next and prev buttons
       navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
-      slideSpeed: 2000,
+      slideSpeed: 3000,
       paginationSpeed: 1000,
-      autoPlay: false,
+      autoPlay: true,
       stopOnHover: true,
       singleItem: true,
       lazyLoad: false,
